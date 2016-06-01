54c90d970d6feefbd5022581fb0d1a0f90162045
AppCoda <Smartios2012>
2016-06-01 22:02:35 +0800

    add pod files

---
+++ QYAliPay/AlipaySDK.h
@@ -0,0 +1,13 @@
+//
+//  AlipaySDK.h
+//  
+//
+//  Created by Smartios2012 on 16/6/1.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlipaySDK : UIView
+
+@end
---
+++ QYAliPay/AlipaySDK.m
@@ -0,0 +1,21 @@
+//
+//  AlipaySDK.m
+//  
+//
+//  Created by Smartios2012 on 16/6/1.
+//
+//
+
+#import "AlipaySDK.h"
+
+@implementation AlipaySDK
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
